b42ff37ceac7cc61154dc52426607b436295cfd4
MinhDuy375 <minhduy37a@gmail.com>
2025-08-28 14:55:40 +0700

    pakega db

---
+++ db/chiasetailieudb.sql
@@ -0,0 +1,238 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: chiasetailieudb
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bai_chia_se`
+--
+
+DROP TABLE IF EXISTS `bai_chia_se`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bai_chia_se` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `loai` enum('tai_lieu','do_an','bai_viet') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tieu_de` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mo_ta` text COLLATE utf8mb4_unicode_ci,
+  `file_upload` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `link_host` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `link_source` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cong_nghe` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_mon_hoc` int DEFAULT NULL,
+  `id_danh_muc` int DEFAULT NULL,
+  `id_nguoi_dung` int NOT NULL,
+  `ngay_tao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ngay_cap_nhat` timestamp NULL DEFAULT NULL,
+  `tom_tat` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`),
+  KEY `id_mon_hoc` (`id_mon_hoc`) /*!80000 INVISIBLE */,
+  KEY `id_danh_muc` (`id_danh_muc`),
+  KEY `bai_chia_se_ibfk_3` (`id_nguoi_dung`),
+  CONSTRAINT `bai_chia_se_ibfk_1` FOREIGN KEY (`id_mon_hoc`) REFERENCES `mon_hoc` (`id`),
+  CONSTRAINT `bai_chia_se_ibfk_2` FOREIGN KEY (`id_danh_muc`) REFERENCES `danh_muc` (`id`),
+  CONSTRAINT `bai_chia_se_ibfk_3` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bai_chia_se`
+--
+
+LOCK TABLES `bai_chia_se` WRITE;
+/*!40000 ALTER TABLE `bai_chia_se` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bai_chia_se` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `binh_luan`
+--
+
+DROP TABLE IF EXISTS `binh_luan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `binh_luan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_bai_chia_se` int DEFAULT NULL,
+  `id_nguoi_dung` int DEFAULT NULL,
+  `noi_dung` text COLLATE utf8mb4_unicode_ci,
+  `ngay_tao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_bai_chia_se` (`id_bai_chia_se`),
+  KEY `id_nguoi_dung` (`id_nguoi_dung`),
+  CONSTRAINT `binh_luan_ibfk_1` FOREIGN KEY (`id_bai_chia_se`) REFERENCES `bai_chia_se` (`id`),
+  CONSTRAINT `binh_luan_ibfk_2` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `binh_luan`
+--
+
+LOCK TABLES `binh_luan` WRITE;
+/*!40000 ALTER TABLE `binh_luan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `binh_luan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `danh_muc`
+--
+
+DROP TABLE IF EXISTS `danh_muc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `danh_muc` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_danh_muc` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mo_ta` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `danh_muc`
+--
+
+LOCK TABLES `danh_muc` WRITE;
+/*!40000 ALTER TABLE `danh_muc` DISABLE KEYS */;
+/*!40000 ALTER TABLE `danh_muc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mon_hoc`
+--
+
+DROP TABLE IF EXISTS `mon_hoc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mon_hoc` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_mon` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mo_ta` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mon_hoc`
+--
+
+LOCK TABLES `mon_hoc` WRITE;
+/*!40000 ALTER TABLE `mon_hoc` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mon_hoc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nguoi_dung`
+--
+
+DROP TABLE IF EXISTS `nguoi_dung`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nguoi_dung` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_dang_nhap` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mat_khau` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ho_ten` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `vai_tro` enum('quan_tri_vien','nguoi_dung','khach') COLLATE utf8mb4_unicode_ci DEFAULT 'khach',
+  `ngay_tao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ngay_cap_nhat` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `trang_thai` enum('hoạt_dong','khoa') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ten_dang_nhap` (`ten_dang_nhap`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nguoi_dung`
+--
+
+LOCK TABLES `nguoi_dung` WRITE;
+/*!40000 ALTER TABLE `nguoi_dung` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nguoi_dung` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `thu_vien_ca_nhan`
+--
+
+DROP TABLE IF EXISTS `thu_vien_ca_nhan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `thu_vien_ca_nhan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_nguoi_dung` int DEFAULT NULL,
+  `id_bai_chia_se` int DEFAULT NULL,
+  `loai` enum('tai_lieu','do_an') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ngay_tao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_nguoi_dung` (`id_nguoi_dung`),
+  KEY `id_bai_chia_se` (`id_bai_chia_se`),
+  CONSTRAINT `thu_vien_ca_nhan_ibfk_1` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id`),
+  CONSTRAINT `thu_vien_ca_nhan_ibfk_2` FOREIGN KEY (`id_bai_chia_se`) REFERENCES `bai_chia_se` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `thu_vien_ca_nhan`
+--
+
+LOCK TABLES `thu_vien_ca_nhan` WRITE;
+/*!40000 ALTER TABLE `thu_vien_ca_nhan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `thu_vien_ca_nhan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tuong_tac`
+--
+
+DROP TABLE IF EXISTS `tuong_tac`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tuong_tac` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_bai_chia_se` int DEFAULT NULL,
+  `id_nguoi_dung` int DEFAULT NULL,
+  `loai` enum('like','dislike','share') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ngay_tao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_bai_chia_se` (`id_bai_chia_se`,`id_nguoi_dung`,`loai`),
+  KEY `id_nguoi_dung` (`id_nguoi_dung`),
+  CONSTRAINT `tuong_tac_ibfk_1` FOREIGN KEY (`id_bai_chia_se`) REFERENCES `bai_chia_se` (`id`),
+  CONSTRAINT `tuong_tac_ibfk_2` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tuong_tac`
+--
+
+LOCK TABLES `tuong_tac` WRITE;
+/*!40000 ALTER TABLE `tuong_tac` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tuong_tac` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-08-28 14:54:39
