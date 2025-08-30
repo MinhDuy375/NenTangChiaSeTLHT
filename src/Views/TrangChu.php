98d9f7f62b9d41ba518a9ac463a0d367c4f18e29
MinhDuy375 <minhduy37a@gmail.com>
2025-08-31 00:51:01 +0700

    create ChuyenNganh file

---
+++ src/Views/TrangChu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Trang chủ</p>
+    <div>
+        <a href="./ChuyenNganh.php">Chuyên ngành</a>
+        <a href="./KhoDoAn.php">Kho Đồ án</a>
+    </div>
+</body>
+</html>
